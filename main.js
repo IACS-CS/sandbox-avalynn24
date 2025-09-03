28b6d87798a5ba76641b35d6a08c4c4dcb51d30a
avalynn24 <avalynn.annarelli@innovationcharter.org>
2025-09-03 16:25:57 +0000

    made changes

--- main.js
+++ main.js
@@ -5,8 +5,8 @@ let app = document.querySelector("#app");
 // Create a new "Text Interface"
 let ti = new TextInterface(app, "Example Text Interface Program");
 
-ti.output("Welcome to my program!");
-let name = await ti.prompt("What is your name?");
+ti.output("Welcome to my program!!!!");
+let name = await ti.prompt("Hello, what is your name?");
 ti.output("Hello, " + name);
-
-
+ti.output("I am program")
+let likesbikes = await ti.promptYesOrNo("do you like bikes?")
