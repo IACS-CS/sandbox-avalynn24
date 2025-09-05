f01bbf2c00cb637b7a9c715122a285473b15b130
avalynn24 <avalynn.annarelli@innovationcharter.org>
2025-09-05 18:35:24 +0000

    made changes

--- main.js
+++ main.js
@@ -6,9 +6,27 @@ let app = document.querySelector("#app");
 let ti = new TextInterface(app, "Example Text Interface Program");
 
 ti.output("Welcome to my program!!!!");
-let name = await ti.prompt("Hello, what is your name?");
-ti.output("Hello, " + name);
-ti.output("I am program")
-let likesbikes = await ti.promptYesOrNo("do you like bikes?")
-if (likesbikes){
-ti.output("good, you better");}
+let name = await ti.prompt("What is your name?");
+if (name === "Avalynn" || name === "Ava"||name==="avalynn"||name==="ava") {
+  ti.output("Oh, hi God");
+} else {
+  ti.output("Hello, " + name);
+  ti.output("I have some random queations");
+  let KeepRepeating = true;
+  while (KeepRepeating) {
+    let color = await ti.prompt("what color do you assosiate with math?");
+    if (color === "Red" || color=== "red") {
+      ti.output("Correct!");
+      KeepRepeating= false
+    } else {
+      ti.output("WRONG, try again");
+    }
+  }ti.output("next queation");
+  KeepRepeating = true;
+  if (KeepRepeating){
+  let color = await ti.prompt("what color do you assosiate with English?")
+  if (color=== "Blue"|| color=== "blue"){
+    ti.output("Correct!");
+  }
+}
+}
